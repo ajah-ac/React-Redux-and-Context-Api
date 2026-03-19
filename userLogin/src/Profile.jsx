b7896417be1ddce0ae874475d6980fe2070b8ac4
ajah <ajahajang00@gmail.com>
2026-03-19 14:39:36 +0200

    Initial Commit:SetUp

---
+++ userLogin/src/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div>Profile
+            <h1>Name:</h1>
+            <h1>Age:</h1>
+            <h1>Email:</h1>
+
+        </div>
+    )
+}
+
+export default Profile
