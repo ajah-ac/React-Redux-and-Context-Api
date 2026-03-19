818fa95c7e34d4c7f125e95737bd8a0f81539eb7
ajah <ajahajang00@gmail.com>
2026-03-19 17:39:39 +0200

    Initial SetUp

---
+++ TodoApp/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+TodoApp
+<h1 className='bg-green-400'>hi</h1>
+    </>
+  )
+}
+
+export default App
