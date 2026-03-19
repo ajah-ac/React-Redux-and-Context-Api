2e03449763272cbbab03a3fd1aa0f34bcf6aff76
ajah <ajahajang00@gmail.com>
2026-03-19 14:57:31 +0200

    Create store and Provide

--- userLogin/src/main.jsx
+++ userLogin/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import userReducer from './features/user'
+const store = configureStore({
+  reducer: {
+user:userReducer
+  }
+})
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <App />
+
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
