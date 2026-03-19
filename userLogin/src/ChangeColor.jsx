6b8090ec17b89ca6a43a53256499c380b454c75c
ajah <ajahajang00@gmail.com>
2026-03-19 15:26:32 +0200

    Add color change component

---
+++ userLogin/src/ChangeColor.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from 'react'
+
+const ChangeColor = () => {
+    const {color,setColor}=useState('')
+  return (
+    <div><input type="text" name="" id="" onChange={(e)=>setColor(e.target.value)} />
+    <button>Change Color</button></div>
+  )
+}
+
+export default ChangeColor
