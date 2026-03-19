56457c69c663f3776877c45824a3e60aa67ae868
ajah <ajahajang00@gmail.com>
2026-03-19 18:05:56 +0200

    Add Input field component

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -3,13 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './output.css'
-
+import Todo from './components.jsx/Todo'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Todo/>
     </>
   )
 }
--- TodoApp/src/components.jsx/Todo.jsx
+++ TodoApp/src/components.jsx/Todo.jsx
@@ -1,9 +1,11 @@
+import { Plus } from 'lucide-react'
 import React from 'react'
 
 const Todo = () => {
   return (
-    <div className='bg-green-200'>
-        <input type="text" />
+    <div className='bg-green-200 p-4 rounded-3xl w-3/4 m-auto flex justify-between'>
+        <input type="text" className='w-3/4 border-0 outline-0' placeholder='hello world'/>
+        <button className='bg-red-200'> <Plus/></button>
     </div>
   )
 }
