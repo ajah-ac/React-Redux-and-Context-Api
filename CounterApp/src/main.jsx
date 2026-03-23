1c7b111cb5dfc105744723c0049512bc4aeeef83
ajah <ajahajang00@gmail.com>
2026-03-23 14:58:38 +0200

    Configure and Provide Store

--- CounterApp/src/main.jsx
+++ CounterApp/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {Provider} from 'react-redux'
 import App from './App.jsx'
+import {configureStore} from '@reduxjs/toolkit'
+import CounterReducer from './counter.js'
+const store=configureStore({
+  reducer:{
+counter:CounterReducer
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+
+<Provider store={store}>
+      <App />
+
+  </Provider>  </StrictMode>,
 )
