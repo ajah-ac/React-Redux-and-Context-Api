1ab6088c7d2725c3b52a3e005ed8e330f6255cbd
ajah <ajahajang00@gmail.com>
2026-03-23 16:07:34 +0200

    Adjust css

--- TodoApp/src/components/AddTodo.jsx
+++ TodoApp/src/components/AddTodo.jsx
@@ -7,13 +7,13 @@ const AddTodo = () => {
     const [input,setInput]=useState('')
     const dispatch=useDispatch()
   return (
-    <div className='bg-green-200 p-4 rounded-3xl w-3/4 m-auto flex justify-between'>
+    <div className='shadow-md mt-40 p-4 rounded-3xl w-3/4 m-auto flex justify-between'>
         <input type="text" 
         className='w-3/4 border-0 outline-0'
-         placeholder='hello world'
+         placeholder='Add a task'
          onChange={e=>setInput(e.target.value)}
          />
-        <button className='bg-red-200' onClick={()=>{dispatch(addTodo(input))}}> <Plus/></button>
+        <button className='' onClick={()=>{dispatch(addTodo(input))}}> <Plus/></button>
     </div>
   )
 }
