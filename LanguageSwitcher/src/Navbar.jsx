76c62fce1544bb0ff9c33120f63cfa9d193aa2de
ajah <ajahajang00@gmail.com>
2026-03-24 16:37:17 +0200

    Navbar Section

---
+++ LanguageSwitcher/src/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <ul className='flex  mt-4 justify-evenly  shadow-md p-4 rounded-3xl '>
+        
+        <li><a href="">Home</a></li>
+        <li><a href="">Services</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Contact</a></li>
+        
+
+    </ul>
+  )
+}
+
+export default Navbar
