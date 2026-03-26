730b68c8d30553e1a2eea3e183ae1726501c6a59
ajah <ajahajang00@gmail.com>
2026-03-26 16:09:28 +0200

    Increase Product Quantity in cart

--- ShoppingCart/src/Cart.jsx
+++ ShoppingCart/src/Cart.jsx
@@ -5,20 +5,20 @@ const Cart = () => {
   return (
     <>
         <ul>
-            { items &&  items.map(item=><li key={item.id}><CartItem image={item.image} name={item.name} price={item.price}/></li>)}
+            { items &&  items.map(item=><li key={item.id}><CartItem image={item.image} quantity={item.quantity} name={item.name} price={item.price}/></li>)}
         </ul>
     </>
   )
 }
 
 export default Cart
-const CartItem =({id,name,image,price})=>{
+const CartItem =({quantity,name,image,price})=>{
     return (
         <>
         <div className='flex'>
 <div className='max-w-10 flex '><img src={image} alt="image here" /></div>
             <div className=''><h2>{name}</h2>
-                <span>{price}</span></div></div>
+                <span>{price}</span> <span>{quantity}</span></div></div>
 
                     </>
     )
--- ShoppingCart/src/produceSlice.js
+++ ShoppingCart/src/produceSlice.js
@@ -3,8 +3,29 @@ const productSlice=createSlice({
     name:'product',
     initialState:{items:[]},
     reducers:{
-        addToCart:(state,action)=>{
-state.items.push(action.payload)
+        addToCart:{
+            reducer(state,action){
+                const item=action.payload
+                const exists=state.items.find((p=>p.id===item.id))
+if(exists){
+    exists.quantity+=1
+}
+else{
+    state.items.push(item)
+}
+ 
+
+
+},
+prepare({id,name,price,image}){
+    return{
+    payload:{
+id,name,
+price,image,
+quantity:1
+    }
+    }
+}
         }
     }
 })
